b95870c22f4a2fc5e6c276f9cd8383aefc99f950
iammoussaab <iammoussaab@gmail.com>
2025-05-18 11:05:38 +0100

    Implement user management features and update Angular configuration

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'tp5';
+  users: string[] = ['John', 'Jane', 'Jim', 'Jill'];
+  newUserName: string = '';
+
+  removeUser() {
+    this.users.pop();
+  }
+  clearUser() {
+    this.users = [];
+  }
+
+  addUser(name: string) {
+    this.users.push(name);
+  }
+
 }
